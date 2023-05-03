78ca14147552f5b39ebce5ac2eb767094e457fab
Rodrigo de Lascio <rodrigodelascio@gmail.com>
2023-05-03 17:45:56 +0100

    pagination fully implemented

--- src/js/controller.js
+++ src/js/controller.js
@@ -2,6 +2,7 @@ import * as model from "./model.js";
 import recipeView from "./views/recipeView.js";
 import searchView from "./views/searchView.js";
 import resultsView from "./views/resultsView.js";
+import paginationView from "./views/paginationView.js";
 
 import "core-js/stable";
 import "regenerator-runtime/runtime";
@@ -47,6 +48,8 @@ const controlSearchResults = async function () {
 
     resultsView.render(model.getSearchResultsPage());
 
+    paginationView.render(model.state.search);
+
   } catch (err) {
 
     console.log(err);
@@ -55,10 +58,19 @@ const controlSearchResults = async function () {
 
 };
 
+const controlPagination = function (goToPage) {
+
+  resultsView.render(model.getSearchResultsPage(goToPage));
+
+  paginationView.render(model.state.search);
+
+};
+
 const init = function () {
 
   recipeView.addHandlerRender(controlRecipes);
   searchView.addHandlerSearch(controlSearchResults);
+  paginationView.addHandlerClick(controlPagination);
 
 };
 
