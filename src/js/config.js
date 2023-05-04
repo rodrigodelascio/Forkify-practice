3af658d61a35c61e5fe34dedabd67a9d1124bb47
Rodrigo de Lascio <rodrigodelascio@gmail.com>
2023-05-04 15:55:53 +0100

    uploading user recipes

--- src/js/config.js
+++ src/js/config.js
@@ -1,4 +1,6 @@
 export const API_URL = "https://forkify-api.herokuapp.com/api/v2/recipes/";
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
+export const KEY = "2cdaafe4-d387-4cb9-a047-c7c6f794bf13";
+export const MODAL_CLOSE_SEC = 2.5;
 
--- src/js/controller.js
+++ src/js/controller.js
@@ -1,9 +1,11 @@
 import * as model from "./model.js";
+import { MODAL_CLOSE_SEC } from "./config.js";
 import recipeView from "./views/recipeView.js";
 import searchView from "./views/searchView.js";
 import resultsView from "./views/resultsView.js";
 import paginationView from "./views/paginationView.js";
 import bookmarksView from "./views/bookmarksView.js";
+import addRecipeView from "./views/addRecipeView.js";
 
 import "core-js/stable";
 import "regenerator-runtime/runtime";
@@ -105,6 +107,40 @@ const controlBookmarks = function () {
 };
 
 
+const controlAddRecipe = async function (newRecipe) {
+
+  try {
+
+    addRecipeView.renderSpinner();
+
+    await model.uploadRecipe(newRecipe);
+
+    recipeView.render(model.state.recipe);
+
+    addRecipeView.renderMessage();
+
+    bookmarksView.render(model.state.bookmarks);
+
+    window.history.pushState(null, "", `#${model.state.recipe.id}`);
+
+    setTimeout(function () {
+
+      addRecipeView.toggleWindow();
+
+    }, MODAL_CLOSE_SEC * 1000);
+
+
+  } catch (err) {
+
+    console.error(err);
+
+    addRecipeView.renderError(err.message);
+
+  }
+
+};
+
+
 const init = function () {
 
   bookmarksView.addHandlerRender(controlBookmarks);
@@ -113,6 +149,7 @@ const init = function () {
   recipeView.addHandlerAddBookmark(controlAddBookmark);
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
+  addRecipeView.addHandlerUpload(controlAddRecipe);
 
 };
 
