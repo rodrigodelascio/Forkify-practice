197f7db6180869d2468f33f872e5080388897660
Rodrigo de Lascio <rodrigodelascio@gmail.com>
2023-05-04 13:29:49 +0100

    bookmarking fully implemented

--- src/js/controller.js
+++ src/js/controller.js
@@ -3,6 +3,7 @@ import recipeView from "./views/recipeView.js";
 import searchView from "./views/searchView.js";
 import resultsView from "./views/resultsView.js";
 import paginationView from "./views/paginationView.js";
+import bookmarksView from "./views/bookmarksView.js";
 
 import "core-js/stable";
 import "regenerator-runtime/runtime";
@@ -23,6 +24,7 @@ const controlRecipes = async function () {
     recipeView.renderSpinner();
 
     resultsView.update(model.getSearchResultsPage());
+    bookmarksView.update(model.state.bookmarks);
 
     // LOAD RECIPE
     await model.loadRecipe(id);
@@ -80,13 +82,18 @@ const controlServings = function (newServings) {
 
 const controlAddBookmark = function () {
 
+  // add and remove bookmarks
   if (!model.state.recipe.bookmarked) {
     model.addBookmark(model.state.recipe)
   } else {
     model.deleteBookmark(model.state.recipe.id)
   };
 
+  // render recipe view
   recipeView.update(model.state.recipe);
+
+  // render bookmarks
+  bookmarksView.render(model.state.bookmarks);
 
 };
 
